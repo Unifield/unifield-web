22844a5d5a96cf8e8738696b32a998410a731a08
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2017-05-19 09:11:54 +0200

    US-1026 [IMP] remove useless whitespace

--- addons/openerp/controllers/openo2m.py
+++ addons/openerp/controllers/openo2m.py
@@ -103,7 +103,7 @@ class OpenO2M(Form):
             form = cherrypy.request.terp_form
         else:
             form = self.create_form(params, tg_errors)
-            params.o2m_ids = self.get_o2m_ids(params.parent_model, params.parent_id, params.o2m, params.o2m_model, order=params.view_params[params.o2m].    sort_order)
+            params.o2m_ids = self.get_o2m_ids(params.parent_model, params.parent_id, params.o2m, params.o2m_model, order=params.view_params[params.o2m].sort_order)
             pager = tw.pager.Pager(id=params.o2m_id, ids=params.o2m_ids, offset=0,
                                    limit=-1, count=len(params.o2m_ids), view_type='form',
                                    pager_options=[])
